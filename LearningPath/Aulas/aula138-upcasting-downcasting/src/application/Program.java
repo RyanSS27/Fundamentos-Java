ac00a04048e062a69b9840f4586688c34bc67af2
Ryan de Souza Silva <ryan.souzasilvap1@gmail.com>
2026-03-04 14:13:54 -0300

    Testes

--- LearningPath/Aulas/aula138-upcasting-downcasting/src/application/Program.java
+++ LearningPath/Aulas/aula138-upcasting-downcasting/src/application/Program.java
@@ -16,6 +16,7 @@ public class Program {
         Account acc1 = bacc;
         Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
         Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+        System.out.println("Meu teste");
 
         // DOWNCASTING
 
